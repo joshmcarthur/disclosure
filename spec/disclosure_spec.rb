f1b2bd472a4a0a417176b27d5167f11ab421fa8b
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-31 23:15:04 +1300

    Add passing specs for react_to method

--- spec/disclosure_spec.rb
+++ spec/disclosure_spec.rb
@@ -21,4 +21,56 @@ describe Disclosure do
 
     Disclosure.configuration.owner_class.should eq "Administrator"
   end
+
+  describe "react_to!" do
+
+    let(:model) { Disclosure::Issue.new }
+    let(:rule) { Disclosure::Rule.new.tap { |r| r.action = 'closed' } }
+
+    context "model is not reactable" do
+      let(:model) { String.new }
+
+      it "should not react" do
+        Disclosure::Rule.any_instance.should_not_receive(:react!)
+        subject.react_to!(model)
+      end
+
+      it "should return nil" do
+        subject.react_to!(model).should be_nil
+      end
+    end
+
+    it "should find matching rules" do
+      Disclosure::Rule.any_instance.stub(:react!)
+      Disclosure::Rule.should_receive(:where).with(
+        :notifier_class => "Disclosure::Issue",
+        :owner_id => 1
+      ).and_return([rule])
+      subject.react_to!(model)
+    end
+
+    it "should reject rules whose action does not match" do
+      model.stub(:closed?).and_return(false)
+      rule.should_not_receive :react!
+      Disclosure::Rule.stub(:where).and_return([rule])
+
+      subject.react_to!(model)
+    end
+
+    it "should react to matching rules" do
+      Disclosure::Rule.stub(:where).and_return([rule])
+      rule.should_receive(:react!).once()
+
+      subject.react_to!(model)
+    end
+  end
+
+  describe "subscription" do
+    let(:model) { Disclosure::Issue.new }
+
+    it "should be subscribed to the model event" do
+      subject.should_receive(:react_to!).with(model)
+      model.save
+    end
+  end
 end
