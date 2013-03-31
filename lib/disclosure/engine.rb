3b7e918689975466a7acd15762c4fe5df8874eea
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-31 23:13:20 +1300

    Add initializers: 1. Subscribe to notifications issued by models after saving 2. Bootstrap required methods into models for implementation by the user

--- lib/disclosure/engine.rb
+++ lib/disclosure/engine.rb
@@ -1,5 +1,15 @@
 module Disclosure
   class Engine < ::Rails::Engine
     isolate_namespace Disclosure
+
+    initializer 'disclosure.subscribe_to_model_events' do
+      ActiveSupport::Notifications.subscribe "disclosure.model_saved" do |name, start, finish, id, payload|
+        Disclosure.react_to!(payload[:model])
+      end
+    end
+
+    initializer 'disclosure.extend_model_classes' do
+      Disclosure.bootstrap!
+    end
   end
 end
