bb66d2e195b6561f520bd1092a47b74e52ba5cc2
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-31 23:13:56 +1300

    Add two new methods: 1. bootstrap!, to add required methods to notifiable models 2. react_to!, that accepts a model as an argument and reacts to any    events on that model, if any

--- lib/disclosure.rb
+++ lib/disclosure.rb
@@ -6,6 +6,38 @@ module Disclosure
     attr_accessor :configuration
   end
 
+  def self.bootstrap!
+    Disclosure.configuration.notifier_classes.each do |klass|
+      unless klass.methods.include?(:notifiable_actions)
+        klass.define_method(:notifiable_actions) do
+          raise ::NotImplementedError, "Notifiable actions must be defined in #{klass.name}."
+        end
+      end
+
+      klass.notifiable_actions.each do |action|
+        unless klass.instance_methods.include?(:"#{action}?")
+          klass.define_method(:"#{action}?") do
+            raise ::NotImplementedError, "#{action}? must be defined in #{klass}."
+          end
+        end
+      end
+    end
+  end
+
+  def self.react_to!(model)
+    unless Disclosure.configuration.notifier_classes.include?(model.class) 
+      return nil
+    end
+
+    Disclosure::Rule.where(
+      :notifier_class => model.class.name,
+      :owner_id => model.send("#{Disclosure.configuration.owner_class.underscore}_id")
+    ).each do |rule|
+      next if !model.send("#{rule.action}?")
+      rule.react!(model)
+    end
+  end
+
   def self.configuration
     @configuration ||= Configuration.new
   end
