c11b90d51e59603830f72e0e8fdfae5d0a8d50ef
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-01 14:47:13 +1300

    Attirbution for I18n helper

--- spec/support/locales.rb
+++ spec/support/locales.rb
@@ -1,3 +1,5 @@
+# From https://github.com/plataformatec/devise/blob/master/test/support/helpers.rb
+
 def store_translations(locale, translations, &block)
   begin
     I18n.backend.store_translations(locale, translations)
