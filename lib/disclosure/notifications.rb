293339e93860c66f9dbe32b2582ddcdc7e4fccf7
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-02 22:26:28 +1300

    Update notification to use around_save - this way, the most attributes we are able to access are present

--- lib/disclosure/notifications.rb
+++ lib/disclosure/notifications.rb
@@ -9,12 +9,19 @@ module Disclosure
     def self.instrument!
       Disclosure.configuration.notifier_classes.each do |klass|
         klass.class_eval do
-          after_commit :notify_disclosure
+          around_save :notify_disclosure_with_save
 
           private
 
-          def notify_disclosure
-            ActiveSupport::Notifications.instrument("disclosure.model_saved", :model => self)
+          def notify_disclosure_with_save
+            # Keep track of the original model to pick up changes properly
+            @model_before_save = self
+
+            # Call save, etc.
+            yield
+
+            # Send the notification with the original model
+            ActiveSupport::Notifications.instrument("disclosure.model_saved", :model => @model_before_save)
           end
         end
       end
